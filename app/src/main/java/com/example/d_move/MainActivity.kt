6f565bcccb7324b91941c585cfac1b5782a83d55
MokkeMeguru <meguru.mokke@gmail.com>
2020-06-02 08:31:08 +0900

    first-commit

---
+++ app/src/main/java/com/example/d_move/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.d_move
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Typeface
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.hardware.SensorManager
+import android.util.Log
+import android.widget.CompoundButton
+import android.widget.ImageView
+import android.widget.Switch
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import kotlin.math.absoluteValue
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class MainActivity : AppCompatActivity(), SensorEventListener{
+    private lateinit var sensorManager: SensorManager;
+    private var safehack: Boolean = false
+    private lateinit var textView: TextView
+    private var mAccelCurrent = SensorManager.GRAVITY_EARTH;
+    private var mAccelLast = SensorManager.GRAVITY_EARTH;
+    private var threasthold: Float = 0.05f
+    private lateinit var textInput: TextInputEditText
+    private lateinit var imageView: ImageView
+    private var previous_delta = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d("create", "new asset")
+        setContentView(R.layout.activity_main)
+        imageView = findViewById<ImageView>(R.id.working_view)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        textView = findViewById(R.id.debug_rotation)
+        textInput = findViewById(R.id.inputted_tasks)
+
+        val oopsImage: Bitmap? = "sozai/working.png".getBitmapFromAsset()
+        if (oopsImage != null) {
+            imageView.setImageBitmap(oopsImage)
+        }
+    }
+
+
+    private fun String.getBitmapFromAsset(): Bitmap? {
+        if (assets != null) {
+            try {
+                val realPath = assets.open(this)
+                return BitmapFactory.decodeStream(realPath)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+        return null
+    }
+
+    protected override fun onResume() {
+        super.onResume()
+        var accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_UI)
+   }
+
+    protected override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val gravities: FloatArray
+        if (event != null) {
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+
+                // calculates gravity vector size
+                gravities = event.values.clone()
+                var size = 0.0
+                gravities.forEach {
+                    size += it.toDouble().pow(2.0)
+                }
+                size = sqrt(size)
+
+                // update previous accel
+                mAccelLast = mAccelCurrent
+                mAccelCurrent = size.toFloat()
+
+                // calculates diffs
+                val delta = (mAccelCurrent - mAccelLast).absoluteValue
+
+                if (delta > threasthold) {
+                    if (previous_delta != 0) {
+                        safehack = true
+                        intent = Intent(application, OOPsActivity::class.java)
+                        startActivity(intent)
+                        previous_delta = 0
+                    } else {
+                        previous_delta += 1
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/d_move/OOPsActivity.kt
@@ -0,0 +1,129 @@
+package com.example.d_move
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.net.Uri
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
+import kotlin.math.absoluteValue
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+
+class OOPsActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private var mAccelCurrent = SensorManager.GRAVITY_EARTH
+    private var mAccelLast = SensorManager.GRAVITY_EARTH
+    private var threasthold: Float = 0.05f
+    private var back_info_text: String = ""
+    private var created_at: Long = 0
+    private lateinit var imageView: ImageView
+    private lateinit var source_jorno: TextView
+    private lateinit var back_info: TextView
+    private lateinit var return_button: Button
+    private val soruce_jorno_link: String = "https://dic.nicovideo.jp/a/%E3%83%AF%E3%82%B6%E3%83%83%E3%83%97%E3%82%B8%E3%83%A7%E3%83%AB%E3%83%8E"
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oops)
+        return_button = findViewById(R.id.return_button)
+        return_button.setOnClickListener{
+            this.finish()
+        }
+
+        back_info_text = resources.getString(R.string.back_info)
+        back_info = findViewById(R.id.back_info)
+        source_jorno = findViewById(R.id.source_jorno)
+        source_jorno.movementMethod = LinkMovementMethod.getInstance()
+         source_jorno.setOnClickListener{
+           val browserIntent = Intent(Intent.ACTION_VIEW)
+           browserIntent.data = Uri.parse(soruce_jorno_link)
+            startActivity(browserIntent)
+        }
+        imageView = findViewById<ImageView>(R.id.oops_view)
+        val oopsImage: Bitmap? = "sozai/oops.png".getBitmapFromAsset()
+        if (oopsImage != null) {
+            imageView.setImageBitmap(oopsImage)
+        }
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        created_at = System.currentTimeMillis() / 1000
+
+    }
+
+    private fun String.getBitmapFromAsset(): Bitmap? {
+        if (assets != null) {
+            try {
+                val realPath = assets.open(this)
+                return BitmapFactory.decodeStream(realPath)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+        return null
+    }
+    override fun onResume() {
+        super.onResume()
+        val accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val gravities: FloatArray
+        if (event != null) {
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+
+                // calculates gravity vector size
+                gravities = event.values.clone()
+                var size = 0.0
+                gravities.forEach {
+                    size += it.toDouble().pow(2.0)
+                }
+                size = sqrt(size)
+
+                // update previous accel
+                mAccelLast = mAccelCurrent
+                mAccelCurrent = size.toFloat()
+
+                // show diffs
+                val delta = (mAccelCurrent - mAccelLast).absoluteValue
+                val updated_time =  System.currentTimeMillis() / 1000 - created_at
+                if (delta <= threasthold && updated_time > 15.0) {
+                    back_info.text = "%s [%s]".format(back_info_text, "OK")
+                    this.return_button.visibility = View.VISIBLE
+                    this.finish()
+                    return
+                } else {
+                    this.return_button.visibility = View.INVISIBLE
+
+                   back_info.text = "%s [%d]".format(back_info_text, 15 -  updated_time)
+                }
+                if (delta > threasthold) {
+                    created_at = System.currentTimeMillis() / 1000
+                }
+            }
+        }
+    }
+}
